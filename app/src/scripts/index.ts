1c9e63607fcd6436aa2224825d70ba875ae221cc
Shahriyor <shahriyordeveloping@gmail.com>
2021-08-20 22:49:08 +0300

    sliders

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -63,3 +63,35 @@ window.addEventListener('click', function(e){
     }
   }
 })
+
+// Slider
+$('.main-slider_1').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false
+      }
+    },
+});
+$('.main-slider_2').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false
+      }
+    },
+});
