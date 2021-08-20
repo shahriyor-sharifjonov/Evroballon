e3fdadc195d01bf6610c0ea7ee4a58e845206541
Shahriyor <shahriyordeveloping@gmail.com>
2021-08-20 19:14:33 +0300

    header adaptation

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -1,7 +1,6 @@
 // Header Menu
-const headerButton: HTMLButtonElement =
-  document.querySelector(".header__button");
-const headerMenu: HTMLUListElement = document.querySelector(".header__top");
+const headerButton: HTMLButtonElement = document.querySelector(".header__button");
+const headerMenu: HTMLUListElement = document.querySelector(".header__mob");
 let menuOpened = false;
 const menuToggle = () => {
   menuOpened = !menuOpened;
@@ -19,3 +18,48 @@ window.onclick = (e: MouseEvent) => {
   )
     menuToggle();
 };
+
+// Header Mob Menu
+const nav = document.getElementsByClassName('header__mob-nav__link');
+const mobContent = document.getElementsByClassName('header__mob-content');
+const navigation = document.getElementsByClassName('header__mob-nav__navigation');
+
+window.addEventListener('click', function(e){
+  for(let i = 0; i < nav.length; i++){
+    if(e.target == nav[i]){
+      if(!e.target.classList.contains('active')){
+        navigation[0].classList.toggle('right');
+      }
+      for(let i = 0; i < nav.length; i++){
+        nav[i].classList.remove('active');
+      }
+      e.target.classList.add('active');
+    }
+  }
+  if(navigation[0].classList.contains('right')){
+    mobContent[0].classList.remove('active');
+    mobContent[1].classList.add('active');
+  }else{
+    mobContent[1].classList.remove('active');
+    mobContent[0].classList.add('active');
+  }
+})
+
+// Header Mob Drops
+const headerDrop = document.getElementsByClassName('header__mob-drop__btn');
+
+
+window.addEventListener('click', function(e){
+  for(let i = 0; i < headerDrop.length; i++){
+    if(e.target == headerDrop[i]){ 
+      if(!e.target.classList.contains('active')){
+        for(let i = 0; i < headerDrop.length; i++){
+          headerDrop[i].classList.remove('active');
+        }
+        e.target.classList.add('active');
+      }else{
+        e.target.classList.remove('active');
+      }
+    }
+  }
+})
